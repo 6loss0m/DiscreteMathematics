1d4de80bfb52c36d9aa4465452e8c6ef14c08c3c
6loss0m <gsjang0807@gmail.com>
2022-10-21 14:04:06 +0900

    Create 18013307.py
    Assignment1 Submit

---
+++ 18013307.py
@@ -0,0 +1,60 @@
+# n-queen problem
+# 18013307 장은영 EunYoung Jang
+
+print("Enter the number of queens( n >= 4 ) : ",end='')
+queen = int(input())
+total = 0
+
+arr1 = [0 for _ in range(queen)]
+arr2 = []
+# Output1
+def pos(k):
+    # 같은 행에 존재하거나 대각선에 존재하면 False
+    for i in range(k):
+        if arr1[k] == arr1[i] or abs(arr1[k] - arr1[i]) == k - i:
+            return False
+    return True
+def out1(k):
+    global total
+    # 가능하면 결과값 +1
+    if k == queen:
+        total += 1
+    else:
+        # 각 열마다 0 ~ N-1 행에 퀸을 배치
+        for i in range(queen):
+            arr1[k] = i
+            # 가능한 경우 진행
+            if pos(k):
+                out1(k+1)
+out1(0)
+print(total)
+# Output2
+def print_result(result):
+    for i in range(queen):
+        for j in range(queen):
+            if j == arr2[i]:
+                print('1', end=' ')
+            else:
+                print('0', end=' ')
+        print()
+    print()
+def out2(n):
+    global total
+    if len(arr2) == queen:
+        #total = total + 1
+        print_result(arr2)
+        return
+    for i in range(queen):
+        if i not in arr2:
+            mapped = True
+            for j in range(len(arr2)):
+                if i == arr2[j]-n+j or i == arr2[j]+n-j:
+                    mapped = False
+                    break
+            if mapped:
+                arr2.append(i)
+                out2(n+1)
+                arr2.pop()
+for i in range(queen):
+    arr2 = [i]
+    out2(1)
